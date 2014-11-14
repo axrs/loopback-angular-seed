6b0c306cf90f2f16838e658138f3929dfbd8df3d
Alexander Scott <xander@cadcoder.com>
2014-11-14 11:42:53 +1000

    Content for Frameworks

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -16,4 +16,47 @@ angular.module('ngLAS.home', [
   })
 
   .controller('HomeCtrl', function HomeController($scope) {
+
+    $scope.frameworks = {
+      frontend: [
+        {
+          name: 'AngularJS',
+          desc: 'HTML is great for declaring static documents, but it falters when we try to use it for declaring ' +
+          'dynamic views in web-applications. AngularJS lets you extend HTML vocabulary for your application. ' +
+          'The resulting environment is extraordinarily expressive, readable, and quick to develop.',
+          logo: 'assets/logos/angularjs-logo.png',
+          uri: 'https://angularjs.org/'
+        }, {
+          name: 'Semantic UI',
+          desc: 'Semantic empowers designers and developers by creating a language for sharing UI.',
+          logo: 'assets/logos/semantic-ui-logo.png',
+          uri: 'http://semantic-ui.com/'
+        }, {
+          name: 'Bower',
+          desc: 'Web sites are made of lots of things — frameworks, libraries, assets, utilities, and rainbows. ' +
+          'Bower manages all these things for you.',
+          logo: 'assets/logos/bower-logo.png',
+          uri: 'https://bower.io'
+        }
+      ],
+      backend: [{
+        name: 'Node.js',
+        desc: 'Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable ' +
+        'network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight ' +
+        'and efficient, perfect for data-intensive real-time applications that run across distributed devices.',
+        logo: 'assets/logos/nodejs-logo.png',
+        uri: 'https://nodejs.org/'
+      }, {
+        name: 'LoopBack',
+        desc: 'Powerful Node.js framework for creating APIs and easily connecting to backend data sources.',
+        logo: 'assets/logos/loopback-logo.png',
+        uri: 'https://loopback.io/'
+      }, {
+        name: 'Gulp.js',
+        desc: 'The streaming build system (and task runner)',
+        logo: 'assets/logos/gulp-logo.png',
+        uri: 'http://gulpjs.com'
+      }
+      ]
+    };
   });
